8a3c767740d890902528e7b2ce23b1de0a7b10f1
Dejw30 <dawidbiernacki@gmail.com>
2020-08-11 10:18:12 +0200

    proba

--- src/git/Git.java
+++ src/git/Git.java
@@ -11,6 +11,8 @@ public class Git {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
     
 }
